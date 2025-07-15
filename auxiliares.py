541fa45a287de771f13c99359fc066a745fcc247
Agustín <apuglisi@fi.uba.ar>
2025-07-15 00:15:24 -0300

    Fix date

--- auxiliares.py
+++ auxiliares.py
@@ -35,7 +35,7 @@ def seleccionar_url():
 
     dic_urls = {'https://film-grab.com/2014/10/20/chungking-express/' : 'Chungking Express (1994)', 
             'https://film-grab.com/2014/09/25/fallen-angels/' : 'Fallen Angels (1995)',
-            'https://film-grab.com/2014/06/22/as-tears-go-by/' : 'As Tears Go By (1995)',
+            'https://film-grab.com/2014/06/22/as-tears-go-by/' : 'As Tears Go By (1988)',
             'https://film-grab.com/2014/02/19/happy-together/' : 'Happy Together (1997)',
             'https://film-grab.com/2014/02/17/days-of-being-wild/' : 'Days of Being Wild (1990)',
             'https://film-grab.com/2014/02/21/my-blueberry-nights/' : 'My Blueberry Nights (2007)',
